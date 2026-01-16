06a5e9018dae0bc75fcd985adbf71925712c5599
Ebenehi <148947621+Ebenehi@users.noreply.github.com>
2026-01-16 06:47:55 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:todo_app/screens/intro_screen.dart';
-import 'package:todo_app/screens/tasks_page.dart';
+import 'screens/intro_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter('hive_box');
-  runApp(const MyApp());
+  await Hive.initFlutter('planner_box');
+  runApp(const DailyPlannerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class DailyPlannerApp extends StatelessWidget {
+  const DailyPlannerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Todo App',
+      debugShowCheckedModeBanner: false,
+      title: 'Daily Planner',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-        ),
         useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Todo App'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.blueGrey,
+        ),
+        appBarTheme: const AppBarTheme(
+          centerTitle: false,
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.white,
+          ),
+          iconTheme: IconThemeData(color: Colors.white),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        child: const Icon(Icons.add),
-      ),
+      home: const IntroPage(),
     );
   }
 }
